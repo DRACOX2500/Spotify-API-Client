b5b6606ad92f5914886e9f7feb65ce2ec8426beb
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-11-04 18:21:32 +0100

    fix import

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'auth-spotify.php';
+require_once 'auth-spotify.php';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "https://api.spotify.com/v1/search?q=alestorm&type=artist");
@@ -8,4 +8,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $_SESSION[
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 curl_close($ch);
+
 echo $result;
