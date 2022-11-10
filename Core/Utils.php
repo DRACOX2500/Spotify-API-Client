6ff85e4f98cc3f0dfc231189988d75cc15316cc5
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-11-10 22:18:42 +0100

    update Utils

--- Core/Utils.php
+++ Core/Utils.php
@@ -4,17 +4,15 @@ namespace App\Core;
 
 class Utils
 {
-    public static function millisecondToMinSecFormat(int $milliseconde): string
+    public static function millisecondToMinSecFormat(int $milliseconds): string
     {
 
-        $uSec = $milliseconde % 1000;
-        $input = floor($milliseconde / 1000);
+        $input = floor($milliseconds / 1000);
 
         $seconds = $input % 60;
         $input = floor($input / 60);
 
         $minutes = $input % 60;
-        $input = floor($input / 60);
 
         return $minutes.':'.$seconds;
     }
