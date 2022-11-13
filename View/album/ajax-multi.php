08c7f1790f28c42aedf0702f758882d9ce1b7844
DRACOX2500 <alexis.forestier2001@gmail.com>
2022-11-13 14:06:26 +0100

    add Album Helper

--- View/album/ajax-multi.php
+++ View/album/ajax-multi.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Core\Utils;
 use App\Entity\Album;
+use App\Helper\AlbumHelper;
 
 $albums = $data['albums'];
 
@@ -11,45 +11,6 @@ for ($i = 0; $i < count($albums); $i++) {
     /** @var Album $album */
     $album = $albums[$i];
 
-    $divTracks = '';
-    for ($j = 0; $j < count($album->getTracks()); $j++) {
-        $track = $album->getTracks()[$j];
-        $artistTag = implode(', ', array_map(function ($item) {
-            $artistUrl = $item->getExternalUrl()->getSpotify() ?? '#';
-            return '<a class="link-secondary text-decoration-none hover-underline" href="'.$artistUrl.'" target="_blank">'.$item->getName().'</a>';
-        }, $track->getArtists()));
-
-        $divTracks .= '<tr  id="t-'.$track->getIdSpotify().'">
-                              <th scope="row" class="track-first-index row-track text-center vertical-align-middle">
-                                <span class="pos-num">'.($j + 1).'</span>
-                                <span>
-                                    <button type="button" class="play-btn">
-                                        <i class="bi bi-play-fill fs-4 text-white"></i>
-                                        <i class="bi bi-pause-fill fs-4 text-white d-none"></i>
-                                    </button>
-                                    <audio controls class="play-audio d-none">
-                                        <source src="'.($track->getPreviewUrl() ?? '#').'" />
-                                    </audio>
-                                </span>
-                              </th>
-                              <td  class="d-flex flex-row align-items-center justify-content-between">
-                                    <div class="d-flex flex-column justify-content-between">
-                                        <a class="link-light text-decoration-none" href="'.$track->getExternalUrls()->getSpotify().'" target="_blank">'.$track->getName().'</a>
-                                        <div class="text-secondary">'.$artistTag.'</div>
-                                    </div>
-                                    <button type="button" class="like-track-btn '.($track->isFavorite() ? 'track-fav' : '').'">
-                                        <i class="bi bi-heart '.($track->isFavorite() ? 'd-none' : '').'"></i>
-                                        <i class="bi bi-heart-fill secondary-color '.($track->isFavorite() ? '' : 'd-none').'"></i>
-                                    </button>
-                              </td>
-                              <td class="vertical-align-middle">'.Utils::millisecondToMinSecFormat($track->getDurationMs()).'</td>
-                       </tr>';
-    }
-
-    $artistTag = implode(' • ', array_map(function ($item) {
-        $artistUrl = $item->getExternalUrl()->getSpotify() ?? '#';
-        return '<a class="link-light text-decoration-none hover-underline fw-bold" href="'.$artistUrl.'" target="_blank">'.$item->getName().'</a>';
-    }, $album->getArtists()));
 
     $divs .= '<div class="accordion-item bg-main">
                         <h2 class="accordion-header" id="headingOne">
@@ -60,35 +21,7 @@ for ($i = 0; $i < count($albums); $i++) {
                         </h2>
                         <div id="collapse-'.$i.'" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                               <div class="accordion-body p-0">
-                                    <div id="a-'.$album->getIdSpotify().'" class="album-data">
-                                        <img src="'.$album->getImages()[0]->getUrl().'" alt="album_picture">
-                                        <div>
-                                             <span class="al-type text-light">'.strtoupper($album->getAlbumType()).'</span>
-                                             <span class="al-name text-light">'.$album->getName().'</span>
-                                             <span>
-                                                '.$artistTag.'
-                                                •
-                                                '.$album->getReleaseDate().'
-                                                •
-                                                '.$album->getTotalTracks().' tracks
-                                             </span>
-                                        </div>
-                                        <button type="button" class="album-fav-btn favorite-button fav-btn-rounded '.($album->isFavorite() ? 'is-fav' : '').'">
-                                            <i class="bi bi-star'.($album->isFavorite() ? '-fill' : '').'"></i>
-                                        </button>
-                                    </div>
-                                    <table class="table grey-color mt-3">
-                                          <thead>
-                                                <tr>
-                                                  <th scope="col" class="text-center">#</th>
-                                                  <th scope="col">TITRE</th>
-                                                  <th scope="col"><i class="bi bi-clock px-2"></i></th>
-                                                </tr>
-                                          </thead>
-                                          <tbody class="tbody-no-border-bottom">
-                                            '.$divTracks.'
-                                          </tbody>
-                                    </table>
+                                    '.AlbumHelper::toHTML($album).'
                               </div>
                         </div>
                   </div>';
